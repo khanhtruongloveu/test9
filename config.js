1569079e3b18381b609c7a284273d0d710331d91
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2021-08-18 00:47:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,1 @@
-const CONFIG = {
-    introTitle: 'Hee luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
-    btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
-    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
-    btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+const CONFIG = { introTitle: 'Hế nhô cậu!🙋‍♂️', introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`, btnIntro: 'oke', title: 'Cậu xem tớ là gì vậy 😙', desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là bạn thân tớ đó nha :> ', btnYes: 'Hơn cả tình bạn luôn ó ò oooo😍😍😍 <33', btnNo: 'Cụt shit của đời tớ💩💩💩 :333', question:'Nói cho tới lý do vì sao tớ lại quan trọng với cậu đii🐱‍🏍🐱‍🏍🐱‍🏍 <3', btnReply: 'Gửi cho tớ 😁', reply: 'Tại vì cậu đẹp trai, hài hước nheee hihi😋 ', mess: 'Tớ biết mà 🥰. Cậu xinh lắm lun áaaa 🙆‍♂️😘 ', messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.❤🧡💛💙💚💜🖤💖💝', btnAccept: 'Okiiiii lun <3', messLink: 'https://www.facebook.com/profile.php?id=100034444426000' //link mess của các bạn. VD: https://m.me/nam.nodemy }
